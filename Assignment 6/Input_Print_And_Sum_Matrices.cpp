42350a5b1ec88faa90ed5008381b918e0b55fb2a
Shagun Sharma <60317723+Shagun15@users.noreply.github.com>
2022-05-10 21:47:34 +0530

    Create Input_Print_And_Sum_Matrices.cpp

---
+++ Assignment 6/Input_Print_And_Sum_Matrices.cpp
@@ -0,0 +1,48 @@
+int main() {
+    int sum[3][3];
+    
+    int A[3][3]={
+        {1, 2 ,3},
+        {2, 3, 4},
+        {3, 4, 5}
+    };
+    int B[3][3]={
+        {2,3,4},
+        {1,2,3},
+        {5,6,7}
+       
+    };
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cin>>A[i][j];
+        }
+    }
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cin>>B[i][j];
+        }
+    }
+        cout<<"Matrix is:\n";
+         for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<A[i][j]<<" ";
+        }
+             cout<<"\n";
+         }
+    cout<<"\n";
+     for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<B[i][j]<<" ";
+        }
+             cout<<"\n";
+         }
+    cout<<"\n";
+    
+     for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            sum[i][j]=A[i][j]+B[i][j];
+            cout<<"\n"<<sum[i][j]<<" ";}
+     }
+    cout<<"\n";
+ return 0;
+}
