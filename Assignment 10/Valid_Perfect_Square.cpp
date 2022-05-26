3862b8a223c41e970ba90dcba6edd2044690eacb
Shagun Sharma <60317723+Shagun15@users.noreply.github.com>
2022-05-26 09:19:37 +0530

    Create Valid_Perfect_Square.cpp

---
+++ Assignment 10/Valid_Perfect_Square.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long int mid;
+        int high =num,low=0;
+        if(num==1)
+            return true;
+        if(num==0)
+            return false;
+        while(low<high)
+        {
+            mid=(low+high)/2;
+            
+            if(mid*mid==num)
+                return true;
+            else if(mid*mid<num)
+            {  
+                low= mid+1;
+                
+            }
+            else if(mid*mid>num)
+            {
+                high = mid ;
+            }
+        }
+        return false;
+    }
+};
