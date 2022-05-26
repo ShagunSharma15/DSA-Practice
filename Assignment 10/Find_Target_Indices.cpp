f1c8c40f1d9ae02f8c107667c741fcb5993cfce1
Shagun Sharma <60317723+Shagun15@users.noreply.github.com>
2022-05-26 09:24:01 +0530

    Create Find_Target_Indices.cpp

---
+++ Assignment 10/Find_Target_Indices.cpp
@@ -0,0 +1,36 @@
+vector<int> count_sort(vector<int>&A)
+{
+     int max = *max_element(A.begin(), A.end());
+        int min = *min_element(A.begin(), A.end());
+        int range = max - min + 1;
+        int n = A.size();
+        vector<int> B(range), C(n);
+        for (int i = 0; i < n; i++)
+            B[A[i] - min]++;
+ 
+        for (int i = 1; i < range; i++)
+            B[i] += B[i - 1];
+ 
+        for (int i = n - 1; i >= 0; i--) {
+            C[B[A[i] - min] - 1] = A[i];
+            B[A[i] - min]--;
+        }
+        return C;
+}
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        vector<int>B=count_sort(nums),ans;
+        for(int i=0;i<B.size();i++)
+        {
+            if(B[i]==target)
+            {
+                ans.push_back(i);
+            }
+            if(B[i]>target)
+                break;
+        }
+       
+        return ans;
+    }
+};
