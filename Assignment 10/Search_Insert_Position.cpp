ccc94ed48ce6c4896238a97d2522d4e9a5d7727d
Shagun Sharma <60317723+Shagun15@users.noreply.github.com>
2022-05-26 09:22:41 +0530

    Create Search_Insert_Position.cpp

---
+++ Assignment 10/Search_Insert_Position.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low=0 ,high=nums.size(),mid,ans;
+        while(low<high)
+        {
+            mid = (low+high)/2;
+            if(nums[mid]<target)
+            {
+                low = mid+1;
+                ans= mid+1;
+                
+            }
+            else if(nums[mid]>target)
+            { high = mid;
+             
+            }
+            else
+                return mid;
+        }
+        return ans;
+    }
+};
