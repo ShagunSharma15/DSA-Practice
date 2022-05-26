d4ceb46458fb4d9eaaf45265df3cae620c237ed3
Shagun Sharma <60317723+Shagun15@users.noreply.github.com>
2022-05-26 09:18:29 +0530

    Create Sqrt(x).cpp

---
+++ Assignment 10/Sqrt(x).cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long int mid,ans =x;
+        int high =x,low=0;
+        if(x==1)
+            return 1;
+        if(x==0)
+            return 0;
+        while(low<high)
+        {
+            mid=(low+high)/2;
+            
+            if(mid*mid==x)
+                return mid;
+            else if(mid*mid<x)
+            {   ans= mid;
+                low= mid+1;
+                
+            }
+            else if(mid*mid>x)
+            {
+                high = mid ;
+            }
+        }
+        return ans;
+    }
+};
